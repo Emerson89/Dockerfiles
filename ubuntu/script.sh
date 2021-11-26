534eec12a3e67aefc455e1e752fb0281c1883020
Emerson <you@example.com>
2021-11-26 19:07:10 -0300

    mod repo

--- ubuntu/script.sh
+++ ubuntu/script.sh
@@ -4,7 +4,7 @@ DOCKER_CONTAINER_NAME="ubuntu"
 DOCKER_IMAGE="ubuntu18"
 
 docker build -t $DOCKER_IMAGE .
-docker run -ti --privileged --name $DOCKER_CONTAINER_NAME -d -p 5000:22 $DOCKER_IMAGE
+docker run -ti --privileged --name $DOCKER_CONTAINER_NAME -d -p 5000:22 $DOCKER_IMAGE "/lib/systemd/systemd"
 
 cd ../centos8/zabbix-agent/ && ansible-playbook -i env/local_docker playbook.yml
 
