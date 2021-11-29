2a24c92b8360ce9f38de00ff3415224cec1765bf
emerson <emerson.cesario50@gmail.com>
2021-11-29 18:26:17 -0300

    mod repo

--- centos8/script.sh
+++ centos8/script.sh
@@ -6,11 +6,11 @@ DOCKER_IMAGE="centos8"
 docker build -t $DOCKER_IMAGE .
 docker run -ti --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name $DOCKER_CONTAINER_NAME -d -p 5000:22 $DOCKER_IMAGE
 
-cd zabbix-agent && ansible-playbook -i env/local_docker playbook.yml --extra-vars "zabbix_version=4.4 zabbix_server_ip=192.168.33.10"
+cd ../../zabbix-agent && ansible-playbook -i env/local_docker playbook.yml --extra-vars "zabbix_version=4.4 zabbix_server_ip=192.168.33.10"
 
-docker exec -it $DOCKER_CONTAINER_NAME /bin/bash
-#docker stop $DOCKER_CONTAINER_NAME
+#docker exec -it $DOCKER_CONTAINER_NAME /bin/bash
+docker stop $DOCKER_CONTAINER_NAME
 
-#docker rm $DOCKER_CONTAINER_NAME
+docker rm $DOCKER_CONTAINER_NAME
 
-#docker rmi $DOCKER_IMAGE
+docker rmi $DOCKER_IMAGE
